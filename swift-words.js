be35c3ec5576d1aa6342abef9e3eb1250e1ecf24
azu <azuciao@gmail.com>
2015-01-08 18:05:30 +0900

    fix(rule): ignore the case

--- swift-words.js
+++ swift-words.js
@@ -15,15 +15,20 @@ module.exports = function (context) {
     var matchWords = translatedWords.map(function (key) {
         return dict[key];
     });
+    var matchRegexp = matchWords.map(function (word) {
+        return new RegExp((word), "i");
+    });
     rule[context.Syntax.Str] = function (node) {
         if (helper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote])) {
             return;
         }
         var text = context.getSource(node);
-        matchWords.forEach(function (word) {
-            if(text.indexOf(word) > -1) {
-                var expectedWord = translatedWords[matchWords.indexOf(word)];
-                context.report(node, new context.RuleError(word + " => " + expectedWord));
+        matchRegexp.forEach(function (WordRegexp, index) {
+            var match = text.match(WordRegexp);
+            if (match) {
+                var matchedString = match[0];
+                var expectedWord = translatedWords[index];
+                context.report(node, new context.RuleError(matchedString + " => " + expectedWord));
             }
         });
     };
